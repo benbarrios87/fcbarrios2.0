d5c8460ffbc2b08f3e631de13fb310e0c51f4363
benbarrios87 <benbarrios87@gmail.com>
2026-06-30 11:28:00 +0200

    Import Header component and update main content

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import { api } from "./api.js";
 import { LeaderboardList } from "./components/leaderboard.js";
+import { Header } from "./components/layout/header.js";
 
 async function init() {
   const tournament = await api.getTournament();
@@ -7,8 +8,7 @@ async function init() {
 
   document.querySelector("#app").innerHTML = `
     <main>
-      <h1>FC Barrios</h1>
-      <p>${tournament.name}</p>
+      ${Header("FC Barrios", tournament.name)}
 
       ${LeaderboardList(leaderboard)}
     </main>
