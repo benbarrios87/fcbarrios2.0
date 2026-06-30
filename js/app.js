60b91f40696e4ba6b80b79cf58ddade781421e22
benbarrios87 <benbarrios87@gmail.com>
2026-06-30 11:34:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,14 +2,16 @@ import { api } from "./api.js";
 import { render } from "./render.js";
 import { LeaderboardList } from "./components/leaderboard.js";
 import { Header } from "./components/layout/header.js";
+import { Button } from "./components/ui/button.js";
 
 async function init() {
   const tournament = await api.getTournament();
   const leaderboard = await api.getLeaderboard();
 
-  render(` `
+  render(` 
     <main>
       ${Header("FC Barrios", tournament.name)}
+  ${Button({ label: "Gå til tips", href: "./pages/tips.html" })}
       ${LeaderboardList(leaderboard)}
     </main>
  `);
