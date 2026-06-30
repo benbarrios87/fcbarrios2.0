937351d3e275bd7524c5d69fa3303374e9c4f8b2
benbarrios87 <benbarrios87@gmail.com>
2026-06-30 11:30:58 +0200

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,1 +1,10 @@
+export function render(html) {
+  const app = document.querySelector("#app");
 
+  if (!app) {
+    console.error("Fant ikke #app-elementet.");
+    return;
+  }
+
+  app.innerHTML = html;
+}
